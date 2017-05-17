12c748cf9469154885d1a1132cf002437c590f4b
Christer Olsen <colsen1991@gmail.com>
2017-05-17 19:23:59 +0800

    fix for 409f9ef;

--- web/js/swreg.js
+++ web/js/swreg.js
@@ -1,5 +1,5 @@
 export default function initServiceWorker() {
-  if ('serviceWorker' in navigator) {
+  if (!module.hot && 'serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.getRegistrations()
         .then(() => navigator.serviceWorker.register('sw.js'))
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,38 +10,20 @@ module.exports = Object.assign({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new CopyWebpackPlugin([
-      {
-        from: 'server',
-        to: '../../server'
-      },
-      {
-        from: 'web/index.html',
-        to: '..'
-      },
-      {
-        from: 'web/50x.html',
-        to: '..'
-      },
-      {
-        from: 'web/old-browser.html',
-        to: '..'
-      },
-      {
-        from: 'web/404.html',
-        to: '..'
-      },
-      {
-        from: 'web/style',
-        to: '../style'
-      },
-      {
-        from: 'web/img',
-        to: '../img'
-      },
-      {
-        from: 'package.json',
-        to: '../..'
-      }
+      { from: 'server', to: '../../server' },
+      { from: 'web/index.html', to: '..' },
+      { from: 'web/50x.html', to: '..' },
+      { from: 'web/old-browser.html', to: '..' },
+      { from: 'web/404.html', to: '..' },
+      { from: 'web/browserconfig.xml', to: '..' },
+      { from: 'web/robots.txt', to: '..' },
+      { from: 'web/sw.js', to: '..' },
+      { from: 'web/humans.txt', to: '..' },
+      { from: 'web/manifest.json', to: '..' },
+      { from: 'web/sitemap.xml', to: '..' },
+      { from: 'web/style', to: '../style' },
+      { from: 'web/img', to: '../img' },
+      { from: 'package.json', to: '../..' }
     ])
   ]
 }, config);
